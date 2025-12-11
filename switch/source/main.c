f29f98d63249df72c9b88b3af4e47eea0f215c79
greg6821-debug <greg6821@gmail.com>
2025-12-11 16:52:46 +0300

    Update main.c 7.5.3

--- switch/source/main.c
+++ switch/source/main.c
@@ -2,9 +2,66 @@
 #include <Python.h>
 #include <stdio.h>
 
+char python_error_buffer[0x400];
+
+void show_error(const char* message, int exit)
+{
+    if (exit == 1) {
+        Py_Finalize();
+    }
+    char* first_line = (char*)message;
+    char* end = strchr(message, '\n');
+    if (end != NULL)
+    {
+        first_line = python_error_buffer;
+        memcpy(first_line, message, (end - message) > sizeof(python_error_buffer) ? sizeof(python_error_buffer) : (end - message));
+        first_line[end - message] = '\0';
+    }
+    ErrorSystemConfig c;
+    errorSystemCreate(&c, (const char*)first_line, message);
+    errorSystemShow(&c);
+    if (exit == 1) {
+        Py_Exit(1);
+    }
+}
+
+u64 cur_progid = 0;
+AccountUid userID={0};
+
 static PyObject* commitsave(PyObject* self, PyObject* args)
 {
+    u64 total_size = 0;
+    u64 free_size = 0;
+    FsFileSystem* FsSave = fsdevGetDeviceFileSystem("save");
+
+    FsSaveDataInfoReader reader;
+    FsSaveDataInfo info;
+    s64 total_entries=0;
+    Result rc=0;
+    
     fsdevCommitDevice("save");
+    fsFsGetTotalSpace(FsSave, "/", &total_size);
+    fsFsGetFreeSpace(FsSave, "/", &free_size);
+    if (free_size < 0x800000) {
+        u64 new_size = total_size + 0x800000;
+
+        fsdevUnmountDevice("save");
+        fsOpenSaveDataInfoReader(&reader, FsSaveDataSpaceId_User);
+
+        while(1) {
+            rc = fsSaveDataInfoReaderRead(&reader, &info, 1, &total_entries);
+            if (R_FAILED(rc) || total_entries==0) break;
+
+            if (info.save_data_type == FsSaveDataType_Account && userID.uid[0] == info.uid.uid[0] && userID.uid[1] == info.uid.uid[1] && info.application_id == cur_progid) {
+                fsExtendSaveDataFileSystem(info.save_data_space_id, info.save_data_id, new_size, 0x400000);
+                break;
+            }
+        }
+
+        fsSaveDataInfoReaderClose(&reader);
+        fsdevMountSaveData("save", cur_progid, userID);
+
+    }
     return Py_None;
 }
 
@@ -65,14 +122,15 @@ PyMODINIT_FUNC init_renpybidi();
 PyMODINIT_FUNC initrenpy_audio_renpysound();
 PyMODINIT_FUNC initrenpy_display_accelerator();
 PyMODINIT_FUNC initrenpy_display_render();
-PyMODINIT_FUNC initrenpy_display_matrix(); 
+PyMODINIT_FUNC initrenpy_display_matrix();
+PyMODINIT_FUNC initrenpy_gl_gl();
 PyMODINIT_FUNC initrenpy_gl_gldraw();
 PyMODINIT_FUNC initrenpy_gl_glenviron_shader();
 PyMODINIT_FUNC initrenpy_gl_glrtt_copy();
 PyMODINIT_FUNC initrenpy_gl_glrtt_fbo();
 PyMODINIT_FUNC initrenpy_gl_gltexture();
-PyMODINIT_FUNC initrenpy_parsersupport();
 PyMODINIT_FUNC initrenpy_pydict();
+PyMODINIT_FUNC initrenpy_parsersupport();
 PyMODINIT_FUNC initrenpy_style();
 PyMODINIT_FUNC initrenpy_styledata_style_activate_functions();
 PyMODINIT_FUNC initrenpy_styledata_style_functions();
@@ -90,6 +148,18 @@ PyMODINIT_FUNC initrenpy_text_ftfont();
 PyMODINIT_FUNC initrenpy_text_textsupport();
 PyMODINIT_FUNC initrenpy_text_texwrap();
 
+PyMODINIT_FUNC initrenpy_compat_dictviews();
+PyMODINIT_FUNC initrenpy_gl2_gl2draw();
+PyMODINIT_FUNC initrenpy_gl2_gl2mesh();
+PyMODINIT_FUNC initrenpy_gl2_gl2mesh2();
+PyMODINIT_FUNC initrenpy_gl2_gl2mesh3();
+PyMODINIT_FUNC initrenpy_gl2_gl2model();
+PyMODINIT_FUNC initrenpy_gl2_gl2polygon();
+PyMODINIT_FUNC initrenpy_gl2_gl2shader();
+PyMODINIT_FUNC initrenpy_gl2_gl2texture();
+PyMODINIT_FUNC initrenpy_uguu_gl();
+PyMODINIT_FUNC initrenpy_uguu_uguu();
+
 // Overide the heap initialization function.
 void __libnx_initheap(void)
 {
@@ -106,7 +176,6 @@ void __libnx_initheap(void)
         size = 0x2000000*16;
 
     Result rc = svcSetHeapSize(&addr, size);
-
     if (R_FAILED(rc) || addr==NULL)
         diagAbortWithResult(MAKERESULT(Module_Libnx, LibnxError_HeapAllocFailed));
 
@@ -118,16 +187,16 @@ void __libnx_initheap(void)
 }
 
 
-Result createSaveData(u64 TitleID, AccountUid userID)
+Result createSaveData()
 {
     NsApplicationControlData g_applicationControlData;
     size_t dummy;
 
-    nsGetApplicationControlData(0x1, TitleID, &g_applicationControlData, sizeof(g_applicationControlData), &dummy);
+    nsGetApplicationControlData(0x1, cur_progid, &g_applicationControlData, sizeof(g_applicationControlData), &dummy);
 
     FsSaveDataAttribute attr;
     memset(&attr, 0, sizeof(FsSaveDataAttribute));
-    attr.application_id = TitleID;
+    attr.application_id = cur_progid;
     attr.uid = userID;
     attr.system_save_data_id = 0;
     attr.save_data_type = FsSaveDataType_Account;
@@ -151,12 +220,9 @@ Result createSaveData(u64 TitleID, AccountUid userID)
 
 void userAppInit()
 {
-
-    fsdevUnmountAll();
+    // fsdevUnmountAll();
 
     Result rc=0;
-    u64 cur_progid = 0;
-    AccountUid userID={0};
     PselUserSelectionSettings settings;
     
     rc = svcGetInfo(&cur_progid, InfoType_ProgramId, CUR_PROCESS_HANDLE, 0);
@@ -180,7 +246,7 @@ void userAppInit()
     if (accountUidIsValid(&userID)) {
         rc = fsdevMountSaveData("save", cur_progid, userID);
         if (R_FAILED(rc)) {
-            rc = createSaveData(cur_progid, userID);
+            rc = createSaveData();
             rc = fsdevMountSaveData("save", cur_progid, userID);
         }
     }
@@ -203,26 +269,6 @@ ConsoleRenderer* getDefaultConsoleRenderer(void)
     return NULL;
 }
 
-char python_error_buffer[0x400];
-
-
-void show_error_and_exit(const char* message)
-{
-    Py_Finalize();
-    char* first_line = (char*)message;
-    char* end = strchr(message, '\n');
-    if (end != NULL)
-    {
-        first_line = python_error_buffer;
-        memcpy(first_line, message, (end - message) > sizeof(python_error_buffer) ? sizeof(python_error_buffer) : (end - message));
-        first_line[end - message] = '\0';
-    }
-    ErrorSystemConfig c;
-    errorSystemCreate(&c, (const char*)first_line, message);
-    errorSystemShow(&c);
-    Py_Exit(1);
-}
-
 
 static AppletHookCookie applet_hook_cookie;
 static void on_applet_hook(AppletHookType hook, void *param)
@@ -239,7 +285,6 @@ static void on_applet_hook(AppletHookType hook, void *param)
          break;
    }
 }
-
 
 
 int main(int argc, char* argv[])
@@ -286,14 +331,13 @@ int main(int argc, char* argv[])
         {"renpy.display.accelerator", initrenpy_display_accelerator},
         {"renpy.display.matrix", initrenpy_display_matrix},
         {"renpy.display.render", initrenpy_display_render},
-
         {"renpy.gl.gldraw", initrenpy_gl_gldraw},
         {"renpy.gl.glenviron_shader", initrenpy_gl_glenviron_shader},
         {"renpy.gl.glrtt_copy", initrenpy_gl_glrtt_copy},
         {"renpy.gl.glrtt_fbo", initrenpy_gl_glrtt_fbo},
         {"renpy.gl.gltexture", initrenpy_gl_gltexture},
-        {"renpy.parsersupport", initrenpy_parsersupport},
         {"renpy.pydict", initrenpy_pydict},
+        {"renpy.arsersupport", initrenpy_parsersupport},
         {"renpy.style", initrenpy_style},
         {"renpy.styledata.style_activate_functions", initrenpy_styledata_style_activate_functions},
         {"renpy.styledata.style_functions", initrenpy_styledata_style_functions},
@@ -311,27 +355,93 @@ int main(int argc, char* argv[])
         {"renpy.text.textsupport", initrenpy_text_textsupport},
         {"renpy.text.texwrap", initrenpy_text_texwrap},
 
+        {"renpy.compat.dictviews", initrenpy_compat_dictviews},
+        {"renpy.gl2.gl2draw", initrenpy_gl2_gl2draw},
+        {"renpy.gl2.gl2mesh", initrenpy_gl2_gl2mesh},
+        {"renpy.gl2.gl2mesh2", initrenpy_gl2_gl2mesh2},
+        {"renpy.gl2.gl2mesh3", initrenpy_gl2_gl2mesh3},
+        {"renpy.gl2.gl2model", initrenpy_gl2_gl2model},
+        {"renpy.gl2.gl2polygon", initrenpy_gl2_gl2polygon},
+        {"renpy.gl2.gl2shader", initrenpy_gl2_gl2shader},
+        {"renpy.gl2.gl2texture", initrenpy_gl2_gl2texture},
+        {"renpy.uguu.gl", initrenpy_uguu_gl},
+        {"renpy.uguu.uguu", initrenpy_uguu_uguu},
+
+
         {NULL, NULL}
     };
+
+    
 
     FILE* sysconfigdata_file = fopen("romfs:/Contents/lib.zip", "rb");
     FILE* renpy_file = fopen("romfs:/Contents/renpy.py", "rb");
 
     if (sysconfigdata_file == NULL)
     {
-        show_error_and_exit("Could not find lib.zip.\n\nPlease ensure that you have extracted the files correctly so that the \"lib.zip\" file is in the same directory as the nsp file.");
+        show_error("Could not find lib.zip.\n\nPlease ensure that you have extracted the files correctly so that the \"lib.zip\" file is in the same directory as the nsp file.", 1);
     }
 
     if (renpy_file == NULL)
     {
-        show_error_and_exit("Could not find renpy.py.\n\nPlease ensure that you have extracted the files correctly so that the \"renpy.py\" file is in the same directory as the nsp file.");
+        show_error("Could not find renpy.py.\n\nPlease ensure that you have extracted the files correctly so that the \"renpy.py\" file is in the same directory as the nsp file.", 1);
     }
 
     fclose(sysconfigdata_file);
-    Py_InitializeEx(0);
-    Py_SetPythonHome("romfs:/Contents/lib.zip");
-    PyImport_ExtendInittab(builtins);
 
+    Py_SetPythonHome("romfs:/Contents");
+    Py_SetProgramName("romfs:/Contents/lib.zip"); // critical for zipimport
+    PyImport_ExtendInittab(builtins);
+    //Py_SetPythonHome("romfs:/Contents/lib.zip");
+    Py_InitializeEx(0);
+
+
+    // Diagnostic block for Python ZIP loading issues
+    PyRun_SimpleString(
+        "import sys, traceback, zipfile\n"
+        "buf = []\n"
+        "\n"
+        "buf.append('--- Python ZIP Diagnostic ---')\n"
+        "\n"
+        "# 1. sys.path\n"
+        "buf.append('sys.path = ' + repr(sys.path))\n"
+        "\n"
+        "# 2. zipimport availability\n"
+        "try:\n"
+        "    import zipimport\n"
+        "    buf.append('zipimport: OK (builtin=%s)' % (hasattr(zipimport, '__file__')==False))\n"
+        "except Exception as e:\n"
+        "    buf.append('zipimport ERROR: ' + repr(e))\n"
+        "    buf.append(traceback.format_exc())\n"
+        "\n"
+        "# 3. Try to open lib.zip\n"
+        "try:\n"
+        "    z = zipfile.ZipFile('romfs:/Contents/lib.zip')\n"
+        "    buf.append('lib.zip: opened OK')\n"
+        "    names = z.namelist()\n"
+        "    buf.append('lib.zip file count: %d' % len(names))\n"
+        "    buf.append('lib.zip sample: ' + repr(names[:20]))\n"
+        "    has_os = any(n.endswith('os.py') or n.endswith('os.pyc') or n.endswith('os.pyo') for n in names)\n"
+        "    buf.append('contains os module: %s' % has_os)\n"
+        "except Exception as e:\n"
+        "    buf.append('lib.zip ERROR: ' + repr(e))\n"
+        "    buf.append(traceback.format_exc())\n"
+        "\n"
+        "# 4. Try to import os\n"
+        "try:\n"
+        "    import os\n"
+        "    buf.append('import os: SUCCESS')\n"
+        "except Exception as e:\n"
+        "    buf.append('import os ERROR: ' + repr(e))\n"
+        "    buf.append(traceback.format_exc())\n"
+        "\n"
+        "# FINAL: join into a single output\n"
+        "error_output = '\\n'.join(buf)\n"
+    );
+    
+    show_error(PyUnicode_AsUTF8(PyObject_GetAttrString(PyImport_ImportModule(\"__main__\"), "error_output")));
+
+
+    
     char* pyargs[] = {
         "romfs:/Contents/renpy.py",
         NULL,
@@ -341,18 +451,18 @@ int main(int argc, char* argv[])
 
     int python_result;
 
-    python_result = PyRun_SimpleString("import sys\nsys.path = ['romfs:/Contents/lib.zip']");
+    python_result = PyRun_SimpleString("import sys\nsys.path.insert(0, 'romfs:/Contents/lib.zip')\nsys.path.insert(0, 'romfs:/Contents')\n");
 
     if (python_result == -1)
     {
-        show_error_and_exit("Could not set the Python path.\n\nThis is an internal error and should not occur during normal usage.");
+        show_error("Could not set the Python path.\n\nThis is an internal error and should not occur during normal usage.", 1);
     }
-
+    
 #define x(lib) \
     { \
         if (PyRun_SimpleString("import " lib) == -1) \
         { \
-            show_error_and_exit("Could not import python library " lib ".\n\nPlease ensure that you have extracted the files correctly so that the \"lib\" folder is in the same directory as the nsp file, and that the \"lib\" folder contains the folder \"python2.7\". \nInside that folder, the file \"" lib ".py\" or folder \"" lib "\" needs to exist."); \
+            show_error("Could not import python library " lib ".\n\nPlease ensure that you have extracted the files correctly so that the \"lib\" folder is in the same directory as the nsp file, and that the \"lib\" folder contains the folder \"python2.7\". \nInside that folder, the file \"" lib ".py\" or folder \"" lib "\" needs to exist.", 1); \
         } \
     }
 
@@ -366,7 +476,7 @@ int main(int argc, char* argv[])
 
     if (python_result == -1)
     {
-        show_error_and_exit("An uncaught Python exception occurred during renpy.py execution.\n\nPlease look in the save:// folder for more information about this exception.");
+        show_error("An uncaught Python exception occurred during renpy.py execution.\n\nPlease look in the save:// folder for more information about this exception.", 1);
     }
 
     Py_Exit(0);

66a701fab2954c9d20ebb5cfb662f9c26fa72fa8
greg6821-debug <greg6821@gmail.com>
2025-12-11 19:54:09 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -65,10 +65,10 @@ rm renpy-$RENPY_VER-sdk.zip
 #unzip -qq rawproject.zip -d raw
 #rm rawproject.zip
 
-rm -rf android-native-symbols renpy_androidlib ./raw/android/lib
-unzip -qq android-native-symbols.zip -d ./raw/android/lib
-rm -rf ./raw/android/lib/x86_64/
-rm android-native-symbols.zip
+#rm -rf android-native-symbols renpy_androidlib ./raw/android/lib
+#unzip -qq android-native-symbols.zip -d ./raw/android/lib
+#rm -rf ./raw/android/lib/x86_64/
+#rm android-native-symbols.zip
 
 pushd renpy-source
 patch -p1 < ../renpy.patch

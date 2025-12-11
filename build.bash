c517d183c282d826a866b673eff43fc2249481b9
greg6821-debug <greg6821@gmail.com>
2025-12-11 03:53:49 +0300

    Update build.bash 7.5.3

--- build.bash
+++ build.bash
@@ -12,11 +12,12 @@ python2 setup.py || true
 PYGAME_SDL2_STATIC=1 python2 setup.py || true
 popd
 
+pushd renpy-source/module
+rm -rf gen gen-static
+
 export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
 export CPPFLAGS="$CFLAGS"
 
-pushd renpy-source/module
-rm -rf gen gen-static
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py || true
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py || true
 popd
@@ -28,10 +29,11 @@ python2 setup.py install_headers
 python2 setup.py install
 popd
 
+pushd renpy-source/module
+
 export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
 export CPPFLAGS="$CFLAGS"
 
-pushd renpy-source/module
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py install
 popd

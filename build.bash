c9f6b72e54751d555ad2d7b8b28be08e46e35072
Gorag <greg6821@gmail.com>
2025-12-17 18:11:51 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -25,11 +25,11 @@ export DEVKITPRO=/opt/devkitpro
 #popd
 
 
-pushd pygame_sdl2-source
-python2 setup.py build
-python2 setup.py install_headers
-python2 setup.py install
-popd
+#pushd pygame_sdl2-source
+#python2 setup.py build
+#python2 setup.py install_headers
+#python2 setup.py install
+#popd
 
 
 pushd renpy-source/module

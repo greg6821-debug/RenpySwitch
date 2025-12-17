2fb251f5286a58ac5906cbe15e67037245d2de01
Gorag <greg6821@gmail.com>
2025-12-17 18:38:58 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -4,13 +4,13 @@ export DEVKITPRO=/opt/devkitpro
 pushd pygame_sdl2-source
 rm -rf gen gen-static
 python2 setup.py || true
-PYGAME_SDL2_STATIC=1 python2 setup.py || true
-popd
+PYGAME_SDL2_STATIC=1 python2 setup.py
+
 
 pushd renpy-source/module
 rm -rf gen gen-static
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py || true
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py || true
+RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py 
+RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py 
 popd
 
 

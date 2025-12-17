bc4305bd878eba1d0ce22fc7a0480b935f1131e0
Gorag <greg6821@gmail.com>
2025-12-17 21:24:35 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,29 +1,29 @@
 set -e
-
+echo "=== ---1--- ==="
 export DEVKITPRO=/opt/devkitpro
 pushd pygame_sdl2-source
 rm -rf gen gen-static
 python2 setup.py build
 PYGAME_SDL2_STATIC=1 python2 setup.py install
 popd
-
+echo "=== ---2--- ==="
 pushd renpy-source/module
 rm -rf gen gen-static
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py install
 popd
-
+echo "=== ---3--- ==="
 pushd pygame_sdl2-source
 python2 setup.py build
 python2 setup.py install_headers
 python2 setup.py install
 popd
-
+echo "=== ---4--- ==="
 pushd renpy-source/module
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build
 RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py install
 popd
-
+echo "=== ---5--- ==="
 
 bash link_sources.bash
 

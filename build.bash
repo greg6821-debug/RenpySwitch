485ba84130aadfbdc460f13c88a4650236f067b3
Gorag <greg6821@gmail.com>
2025-12-17 17:52:56 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -27,19 +27,28 @@ popd
 
 pushd pygame_sdl2-source
 python2 setup.py build
-#python2 setup.py install_headers
-#python2 setup.py install
+python2 setup.py install_headers
+python2 setup.py install
 popd
 
+
 pushd renpy-source/module
+rm -rf gen gen-static
 export PYTHONPATH=$(pwd)/..
 export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
 export CPPFLAGS="$CFLAGS"
+python2 setup.py build_ext --inplace
+popd
+
+#pushd renpy-source/module
+#export PYTHONPATH=$(pwd)/..
+#export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
+#export CPPFLAGS="$CFLAGS"
 
 #RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build
 #RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py install
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build_ext --inplace
-popd
+#RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build_ext --inplace
+#popd
 
 
 bash link_sources.bash

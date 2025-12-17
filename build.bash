82f869e1753ce3ea50657c4939e9c99c32251d6f
Gorag <greg6821@gmail.com>
2025-12-17 17:23:40 +0300

    Comment out unused build commands in build.bash
    Comment out deprecated build steps for renpy-source and pygame_sdl2.

--- build.bash
+++ build.bash
@@ -12,25 +12,27 @@ python2 setup.py || true
 PYGAME_SDL2_STATIC=1 python2 setup.py || true
 popd
 
-pushd renpy-source/module
-rm -rf gen gen-static
 
-export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
-export CPPFLAGS="$CFLAGS"
 
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py 
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py 
-popd
+#pushd renpy-source/module
+#rm -rf gen gen-static
+
+#export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
+#export CPPFLAGS="$CFLAGS"
+
+#RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py build
+#RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py build
+#popd
 
 
 pushd pygame_sdl2-source
 python2 setup.py build
-python2 setup.py install_headers
-python2 setup.py install
+#python2 setup.py install_headers
+#python2 setup.py install
 popd
 
 pushd renpy-source/module
-
+export PYTHONPATH=$(pwd)/..
 export CFLAGS="-I$(pwd)/fribidi-src/lib -I$(pwd)/fribidi-src $CFLAGS"
 export CPPFLAGS="$CFLAGS"
 

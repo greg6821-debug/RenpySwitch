3083306def3f750764f83b2e816a527dbfdab92f
Gorag <greg6821@gmail.com>
2025-12-24 22:53:23 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -5,16 +5,16 @@ pushd pygame_sdl2-source
 rm -rf gen gen-static
 #python2 setup.py || true
 #PYGAME_SDL2_STATIC=1 python2 setup.py || true
-python3 setup.py || true
-PYGAME_SDL2_STATIC=1 python3 setup.py || true
+python3 setup.py build #|| true
+PYGAME_SDL2_STATIC=1 python3 setup.py build #|| true
 popd
 
 pushd renpy-source/module
 rm -rf gen gen-static
 # RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python2 setup.py || true
 # RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python2 setup.py || true
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python3 setup.py
-RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python3 setup.py
+RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local python3 setup.py build
+RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 python3 setup.py build
 popd
 
 

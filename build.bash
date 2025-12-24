923d0ede5776b2a21438c479cc632dc642325285
Gorag <greg6821@gmail.com>
2025-12-25 00:42:48 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -80,6 +80,9 @@ mkdir build
 pushd build
 cmake ..
 make
+echo "===== PyInit symbols ====="
+nm CMakeFiles/renpy-switch.dir/source/module/*.o | grep PyInit || true
+echo "===== PyInit symbols ====="
 popd
 popd
 

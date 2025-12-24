f72b7472f9632410674117cd23d0e564b667356d
Gorag <greg6821@gmail.com>
2025-12-24 21:32:23 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,45 +1,61 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
-export RENPY_VER=7.6.1
+export RENPY_VER=8.3.7
 export PYGAME_SDL2_VER=2.1.0
 
 apt-get -y update
 apt-get -y upgrade
 
+# apt -y install build-essential checkinstall
+# apt -y install libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+# apt -y install python2 python2-dev
+
+# python2 --version
+
+# curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+# python2 get-pip.py
+# pip2 --version 
+
+# apt-get -y install p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
+# pip2 uninstall distribute
+# pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36 setuptools==0.9.8
+
+
+
 apt -y install build-essential checkinstall
 apt -y install libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
-apt -y install python2 python2-dev
-
-python2 --version
-
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-python2 get-pip.py
-pip2 --version 
+apt -y install python3 python3-dev python3-pip
 
 apt-get -y install p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
-pip2 uninstall distribute
-pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36 setuptools==0.9.8
+pip3 uninstall distribute
+pip3 install future six typing requests ecdsa pefile Cython==3.0.12 setuptools
+
+python3 --version
+
+
+
 
 #apt-get -y install python2.7 python-pip p7zip-full cython libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
 #pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36
 
 curl -LOC - https://github.com/greg6821-debug/scripts/releases/download/1.0-scripts/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
-curl -LOC - https://github.com/greg6821-debug/scripts/releases/download/1.0-scripts/python27-switch.tar.gz
+curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/python39-switch.zip
 curl -LOC - https://github.com/greg6821-debug/scripts/releases/download/1.0-scripts/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
 dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
-tar -xf python27-switch.tar.gz -C $DEVKITPRO/portlibs/switch
+unzip -qq python39-switch.zip -d $DEVKITPRO/portlibs/switch
 
 rm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
 rm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
-rm python27-switch.tar.gz
+rm python39-switch.zip
 
 /bin/bash -c 'sed -i'"'"'.bak'"'"' '"'"'s/set(CMAKE_EXE_LINKER_FLAGS_INIT "/set(CMAKE_EXE_LINKER_FLAGS_INIT "-fPIC /'"'"' $DEVKITPRO/switch.cmake'
 
 
-curl -LOC - https://www.renpy.org/dl/$RENPY_VER/pygame_sdl2-$PYGAME_SDL2_VER-for-renpy-$RENPY_VER.tar.gz
+curl -LOC - https://www.renpy.org/dl/$RENPY_VER/pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER.tar.gz
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/renpy-$RENPY_VER-sdk.zip
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/renpy-$RENPY_VER-source.tar.bz2
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/android-native-symbols.zip

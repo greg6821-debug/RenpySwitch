067f141bb2f0f2493af8cb68d4973eb8b1f31423
Gorag <greg6821@gmail.com>
2026-01-12 02:08:37 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -17,7 +17,6 @@ rm -rf gen
 popd
 
 pushd renpy-source/module
-rm -rf gen3 gen3-static build
 # СОЗДАЕМ ДИРЕКТОРИЮ ПЕРЕД СБОРКОЙ
 mkdir -p renpy/audio
 mkdir -p renpy/styledata
@@ -30,17 +29,53 @@ RENPY_DEPS_INSTALL=/usr/lib/x86_64-linux-gnu:/usr:/usr/local RENPY_STATIC=1 pyth
 rm -rf gen
 popd
 
+# rsync -avm --include='*/' --include='*.c' --exclude='*' pygame_sdl2-source/ source/module
+# rsync -avm --include='*/' --include='*.c' --exclude='*' renpy-source/module/ source/module
+# find source/module -mindepth 2 -type f -exec mv -t source/module {} +
+# find source/module -type d -empty -delete
+
+# rsync -avm --include='*/' --include='*.h' --exclude='*' pygame_sdl2-source/ include/module/pygame_sdl2
+# find include/module/pygame_sdl2 -mindepth 2 -type f -exec mv -t include/module/pygame_sdl2 {} +
+# mv include/module/pygame_sdl2/surface.h include/module/pygame_sdl2/src
+# rsync -avm --include='*/' --include='*.h' --exclude='*' renpy-source/module/ include/module
+# mv source/module/hydrogen.c include/module/libhydrogen
+# find include/module -type d -empty -delete
+
+
+
+
+
+# Копируем .c файлы из pygame_sdl2-source
 rsync -avm --include='*/' --include='*.c' --exclude='*' pygame_sdl2-source/ source/module
+
+# Копируем .c файлы из renpy-source/module
 rsync -avm --include='*/' --include='*.c' --exclude='*' renpy-source/module/ source/module
-find source/module -mindepth 2 -type f -exec mv -t source/module {} +
+
+# Перемещаем все файлы из поддиректорий в source/module, не перезаписывая существующие
+find source/module -mindepth 2 -type f -exec mv -n -t source/module {} +
+
+# Удаляем пустые директории
 find source/module -type d -empty -delete
 
+# Копируем .h файлы из pygame_sdl2-source
 rsync -avm --include='*/' --include='*.h' --exclude='*' pygame_sdl2-source/ include/module/pygame_sdl2
-find include/module/pygame_sdl2 -mindepth 2 -type f -exec mv -t include/module/pygame_sdl2 {} +
+
+# Перемещаем все .h файлы из поддиректорий, не перезаписывая существующие
+find include/module/pygame_sdl2 -mindepth 2 -type f -exec mv -n -t include/module/pygame_sdl2 {} +
+
+# Перемещаем surface.h
 mv include/module/pygame_sdl2/surface.h include/module/pygame_sdl2/src
+
+# Копируем .h файлы из renpy-source/module
 rsync -avm --include='*/' --include='*.h' --exclude='*' renpy-source/module/ include/module
+
+# Перемещаем hydrogen.c
 mv source/module/hydrogen.c include/module/libhydrogen
+
+# Удаляем пустые директории
 find include/module -type d -empty -delete
+
+
 
 
 

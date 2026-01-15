f3d6919711f87fec1d2513432f126d62a51bf7ea
Gorag <greg6821@gmail.com>
2026-01-15 14:59:08 +0300

    Update switch_enc.h

--- switch_enc.h
+++ switch_enc.h
@@ -1,20 +1,23 @@
-#ifndef hydro_random_switch_H
-#define hydro_random_switch_H
+#ifndef hydro_random_switch_enc_H
+#define hydro_random_switch_enc_H
 
 #include <switch.h>
-#include <stddef.h>
+#include <string.h>
+
+/*
+ * Only seed the DRBG.
+ * hydro_random_buf() is implemented in random.h
+ */
 
 static int
 hydro_random_init(void)
 {
-    /* libnx RNG doesn't require explicit init */
+    /* Seed initial state using Horizon OS CSPRNG */
+    randomGetBytes(hydro_random_context.state,
+                   sizeof hydro_random_context.state);
+    hydro_random_context.counter = 0;
+    hydro_random_context.available = 0;
     return 0;
-}
-
-static void
-hydro_random_buf(void *buf, size_t size)
-{
-    randomGetBytes(buf, size);
 }
 
 #endif

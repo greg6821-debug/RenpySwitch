1cab01e579e19682ee59428cd1e2e2202d30cb34
Gorag <greg6821@gmail.com>
2026-01-18 05:23:23 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -32,12 +32,6 @@ apt-get -y install libsdl2-dev libsdl2-image-dev libjpeg-dev \
     libmpg123-dev libmodplug-dev libflac-dev libvpx-dev\
     pkg-config autoconf automake libtool
 
-apt -y install \
-    cmake pkg-config nasm yasm \
-    # Дополнительные зависимости для кодеков
-    libx264-dev libx265-dev libvpx-dev libmp3lame-dev libopus-dev libvorbis-dev \
-    libass-dev libfreetype6-dev libsdl2-dev
-
 python3 --version
 
 
@@ -46,58 +40,6 @@ curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/switch-lib
 dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 unzip -qq python39-switch.zip -d $DEVKITPRO/portlibs/switch
 
-
-echo "=== Building FFmpeg for Switch (aarch64) ==="
-
-# 1. Скачивание и распаковка исходников FFmpeg
-FFMPEG_VER="6.1.1"
-cd /tmp
-curl -LO https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VER}.tar.bz2
-tar -xf ffmpeg-${FFMPEG_VER}.tar.bz2
-cd ffmpeg-${FFMPEG_VER}
-
-# 2. Настройка окружения для кросс-компиляции
-export DEVKITPRO=/opt/devkitpro
-export DEVKITA64=${DEVKITPRO}/devkitA64
-export PATH=${DEVKITA64}/bin:$PATH
-
-# 3. Конфигурация для минимальной сборки (можно расширить)
-./configure \
-    --cross-prefix=aarch64-none-elf- \
-    --arch=aarch64 \
-    --target-os=horizon \
-    --enable-cross-compile \
-    --sysroot=${DEVKITA64}/aarch64-none-elf \
-    --prefix=${DEVKITPRO}/portlibs/switch \
-    --pkg-config=pkg-config \
-    --disable-static \
-    --enable-shared \
-    --disable-programs \
-    --disable-doc \
-    --disable-avdevice \
-    --disable-swscale \
-    --disable-postproc \
-    --disable-avfilter \
-    --disable-everything \
-    --enable-decoder=h264,hevc,vp8,vp9,mpeg4,aac,mp3,flac,vorbis,opus \
-    --enable-demuxer=mov,matroska,avi,flv,mpegts \
-    --enable-parser=h264,hevc,vp8,vp9,aac,mpeg4video \
-    --enable-protocol=file \
-    --enable-gpl \
-    --enable-libx264 \
-    --enable-libvpx \
-    --enable-libopus \
-    --extra-cflags="-I${DEVKITPRO}/portlibs/switch/include -O2 -march=armv8-a -mtune=cortex-a57 -ftree-vectorize" \
-    --extra-ldflags="-L${DEVKITPRO}/portlibs/switch/lib"
-
-# 4. Сборка и установка в порты devkitPro
-make -j$(nproc)
-make install
-
-cd ..
-rm -rf ffmpeg-${FFMPEG_VER} ffmpeg-${FFMPEG_VER}.tar.bz2
-
-echo "FFmpeg for Switch installed to ${DEVKITPRO}/portlibs/switch"
 
 
 export LD_LIBRARY_PATH=$DEVKITPRO/portlibs/switch/lib:$LD_LIBRARY_PATH

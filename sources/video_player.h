c27aa58576c745f764450376f616cbf09661009a
Gorag <greg6821@gmail.com>
2026-01-19 00:03:13 +0300

    Update video_player.h

--- sources/video_player.h
+++ sources/video_player.h
@@ -1,13 +1,14 @@
-#pragma once
+#ifndef VIDEO_PLAYER_H
+#define VIDEO_PLAYER_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <SDL2/SDL.h>
 
 void play_video_file(const char *path, int skip_enabled);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif // VIDEO_PLAYER_H
